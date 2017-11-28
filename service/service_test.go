875c0d23ff180d13cf0211580e68bcff79c4c304
URITEST <33456192+URITEST@users.noreply.github.com>
2017-11-28 05:54:52 -0800

    Update service_test.go

--- service/service_test.go
+++ service/service_test.go
@@ -4,8 +4,8 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/circleci/cci-demo-docker/service"
-	"github.com/circleci/cci-demo-docker/test"
+	"github.com/uritest/circleci-docker/service"
+	"github.com/uritest/circleci-docker/test"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
